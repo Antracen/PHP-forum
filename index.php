eff2c931eb7f99dad5608ac2152366c5270fee47
Martin Wass <mawass@kth.se>
2017-05-08 16:01:37 +0200

    Index page, CSS and Postgres-tables

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title> Forum </title>
+		<meta charset="utf-8">
+		<link rel="stylesheet" type="text/css" href="style.css">
+	</head>
+	<body>
+		<center class="welcome_message">
+			Hej där!
+			<br>
+			Denna sida är skapad av Martin Wass.
+		</center>
+	</body>
+</html>
